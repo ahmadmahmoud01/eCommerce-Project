2119250650904cccf8f69f5cf6a45ca4a0b0d490
ahmad <ahmad_mahmoud@gmail.com>
2023-01-13 15:58:04 +0200

    create Cart table

--- resources/views/inc/nav.blade.php
+++ resources/views/inc/nav.blade.php
@@ -8,7 +8,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
+                    <a class="nav-link" href="{{ route('products.index') }}">Home</a>
                 </li>
                 <li>
                     <a class="nav-link" href="#">Orders</a>
--- resources/views/products/search.blade.php
+++ resources/views/products/search.blade.php
@@ -8,11 +8,14 @@
     <div class="col-sm-4 text-center ">
         <h3 class="mb-5">Result For Products</h3>
         @foreach ($products as $product)
-        <div><a href="{{ route('products.show', $product->id) }}">
-            <img src="{{ $product->gallery }}" alt="" width="200px">
-            <h5>{{ $product->name }}</h5>
-            <h5>{{ $product->description }}</h5>
-        </a></div>
+        <hr>
+        <div>
+            <a href="{{ route('products.show', $product->id) }}">
+                <img src="{{ $product->gallery }}" alt="" width="200px">
+                <h6>{{ $product->name }}</h6>
+                <h5>{{ $product->description }}</h5>
+            </a>
+        </div>
         @endforeach
     </div>
 
